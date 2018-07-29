38736ac8017b728c99a520802d5d464ecb18063c
Bernadette Januska <bmjanuska@gmail.com>
2018-07-29 15:33:17 -0400

    Images pause when clicked. Button adds to list of buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,14 @@
 
 
-	//creating div tag to img, rating, etc
-	//creating p tag to store result items
-    //setting the src attribute of the image to a property pullled off the result item
-
-    //append the rating to image and to main div 
-    //append image to main div
-
-    //prepend to page
-
-//============ Pause that GIF! ==============
-	//var that determies state of gif
-	//if and else to determine the state activated and what state it can switch to
 
 
+//============ Array ==============
 
-//array of preloaded gifs
-var gifs = ["typography", "calligraphy", "motion design"];
+//page preloaded gifs
+var gifs = ["ramen", "cake", "pudding"];
 
 
 //============ Functions ==============
-
 
 	//function that adds the array of gifs we have and what we will add
 	function displayGIF() {
@@ -28,7 +16,7 @@ var gifs = ["typography", "calligraphy", "motion design"];
 
 		//============ API ==============
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-		gifDisp + "&api_key=dc6zaTOxFJmzC&limit=3";
+		gifDisp + "&api_key=dc6zaTOxFJmzC&limit=3&rating=pg";
 
 		$.ajax({
 			url: queryURL,
@@ -44,12 +32,7 @@ var gifs = ["typography", "calligraphy", "motion design"];
 
 		 //======= New DIVS ==========
 
-		 //RATING FILTER 
-		 	// if(results[i].rating === "g" || reslts[i].rating ==="pg"){
-
-		 	// }
-
-          //creating div tag
+		 //creating div tag
           var gifDiv = $("<div class='col-3'>");
 
           //creating paragraph tag with the result items rating
@@ -58,8 +41,21 @@ var gifs = ["typography", "calligraphy", "motion design"];
           //Creating and storing and image tag
           var gifImage = $("<img>");
 
+          //attribute for still for later
+          gifImage.attr("data-still", results[i].images.original_still.url);
+
+          //attribute for animate for later
+          gifImage.attr("data-animate", results[i].images.fixed_height.url);
+
           //setting the src attribute of the image to a property pullled off the result item
-          gifImage.attr("src", results[i].images.fixed_height.url);
+          gifImage.attr("src", results[i].images.original_still.url);
+
+          //storing state. on load it is still
+          gifImage.attr("data-state", "still");
+
+          //giving image a class of gif to gifImage
+          gifImage.attr("class", "gif");
+
 
           //appednign the paragraph and image tag ot he gifDiv
           gifDiv.append(p);
@@ -67,7 +63,6 @@ var gifs = ["typography", "calligraphy", "motion design"];
 
           //prepending to page
           $("#imageHuddle").prepend(gifDiv);
-
       }
   });
 	}
@@ -115,4 +110,27 @@ var gifs = ["typography", "calligraphy", "motion design"];
 renderButtons();
 
 
+
+//============ Pause that GIF! ==============
+	//var that determies state of gif
+	//if and else to determine the state activated and what state it can switch to
+    $(document).on("click",".gif", function() {
+
+      //variable named state
+      var state = $(this).attr("data-state");
+
+      // checking the condition if the state is clicked or not
+
+      if (state === "still"){
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      }
+      else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+
+
+ 
+    });
 

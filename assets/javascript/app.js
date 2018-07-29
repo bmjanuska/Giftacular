4c37f91220e636eb33187749645d53d6e3d7ba82
Bernadette Januska <bmjanuska@gmail.com>
2018-07-28 22:34:54 -0400

    Typed out logic and started planning

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+
+
+
+//============ API ==============
+	//======= Variables =========
+	//var for already existing buttons
+	//var for "noun" button
+	//var for queryURL
+
+	//promise?? what part is the promise again? 
+
+
+	//======= New DIVS ==========
+
+	//creating div tag to img, rating, etc
+	//creating p tag to store result items
+    //setting the src attribute of the image to a property pullled off the result item
+
+    //append the rating to image and to main div 
+    //append image to main div
+
+    //prepend to page
+
+//============ Pause that GIF! ==============
+	//var that determies state of gif
+	//if and else to determine the state activated and what state it can switch to
+
+//============ Functions ==============
+	
+	// to render buttons when people make them 
+	//for loop that uses already made array of items 
+
+	//function that add movies when button is clicked 
+	//grab variable from the input / textbox
+	//call render button handles the movie array
+
+
+	//add click listener to elements 
+
+	//render btn
